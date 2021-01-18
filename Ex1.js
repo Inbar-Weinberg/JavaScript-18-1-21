a34d617eae86622aeec344f8cd645790929f5df1
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-18 13:35:15 +0200

    Opened Repo
    opened repo
    finished q1

---
+++ Ex1.js
@@ -0,0 +1,12 @@
+prompt("Question 1")
+let x = Number(prompt("Insert the first number"));
+let y = Number(prompt("Insert the second number"));
+
+if (x > y){
+    alert(x + " is bigger.");
+} else if (x === y){
+    alert("Both numbers are the same.");
+    }
+    else{
+        alert(y + " is bigger;");
+    }
