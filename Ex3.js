b66b061ec8cd8097d8060625c97d712d95565582
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-18 13:59:05 +0200

    Q3

---
+++ Ex3.js
@@ -0,0 +1,23 @@
+"use strict";
+alert("Question 3")
+let x = Number(prompt("Insert the first number"));
+let y = Number(prompt("Insert the second number"));
+let z = Number(prompt("Insert the third number"));
+let big = x;
+let temp;
+if (z > x){
+    temp = x;
+    x = z;
+    z = temp;
+}
+if (y > x){
+    temp = x;
+    x = y;
+    y = temp;
+}
+if (z > y){
+    temp = y;
+    y = z;
+    z = temp;
+}
+alert(x +", " + y + " ," + z)
