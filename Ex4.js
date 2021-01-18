c7d4cae90e381b9ff0bcdfc0e5116666bf3d6269
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-18 14:09:59 +0200

    Q4 complete

--- Ex4.js
+++ Ex4.js
@@ -1,8 +1,16 @@
 "use strict";
 alert("Question 4")
-let a = Number(prompt("Insert the first number"));
-let b = Number(prompt("Insert the second number"));
-let c = Number(prompt("Insert the third number"));
-let d = Number(prompt("Insert the fourth number"));
-let e = Number(prompt("Insert the fifth number"));
-letbig = 
+let arr = [
+    Number(prompt("Insert the first number")),
+    Number(prompt("Insert the second number")),
+    Number(prompt("Insert the third number")),
+    Number(prompt("Insert the fourth number")),
+    Number(prompt("Insert the fifth number"))
+];
+
+let big = arr[0];
+for (let i=1 ; i < arr.length ; i++){
+    if (arr[i] > big)
+    big = arr[i];
+}
+alert(big + " Is the biggest number");
