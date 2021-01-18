6a18217a02b7d57d7faa42ed3855cc44879f86ab
Inbar Weinberg <55811314+Inbar-Weinberg@users.noreply.github.com>
2021-01-18 14:17:41 +0200

    Q5 Done

---
+++ Ex5.js
@@ -0,0 +1,10 @@
+"use strict";
+alert("Question 5")
+for (let i=1 ; i <= 15 ; i++){
+    if (i % 2 === 0){
+        console.log(i + " Is an even number.");
+    }
+    else{
+        console.log(i + " Is an odd number.");
+    }
+}
